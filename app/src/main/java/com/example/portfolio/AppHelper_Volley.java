d0deb82737db17e20a6bf90182c42447d9b73cc8
Jin-Woo Kim <dnehd6739@naver.com>
2020-12-22 21:44:10 +0900

    Volley

---
+++ app/src/main/java/com/example/portfolio/AppHelper_Volley.java
@@ -0,0 +1,11 @@
+package com.example.portfolio;
+
+
+import com.android.volley.RequestQueue;
+
+//볼리 관련 자바 클래스
+public class AppHelper_Volley {
+    public static RequestQueue requestQueue;
+
+
+}
---
+++ app/src/main/java/com/example/portfolio/MyVolley.java
@@ -0,0 +1,77 @@
+package com.example.portfolio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyVolley extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_volley);
+
+        textView = findViewById(R.id.textView15);
+        Button button = findViewById(R.id.button36);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest();
+            }
+        });
+
+        if (AppHelper_Volley.requestQueue == null) {
+            AppHelper_Volley.requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+    }
+
+    public void sendRequest() {
+        String url = "https://www.google.co.kr";
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        println("응답 -  "+ response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        println("에러 ->" + error.getMessage());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+
+                return params;
+            }
+        };
+
+        request.setShouldCache(false);
+        AppHelper_Volley.requestQueue.add(request);
+        println("요청 보냄.");
+
+    }
+
+    public void println(String data) {
+        textView.append(data + "\n");
+    }
+}
