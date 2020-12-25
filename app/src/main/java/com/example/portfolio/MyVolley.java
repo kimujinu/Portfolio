f3a1c95f832f970ce5cf131a48f822ef7de5c570
Jin-Woo Kim <dnehd6739@naver.com>
2020-12-26 02:03:13 +0900

    Image Download

--- app/src/main/java/com/example/portfolio/MyVolley.java
+++ app/src/main/java/com/example/portfolio/MyVolley.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -20,6 +21,7 @@ import java.util.Map;
 
 public class MyVolley extends AppCompatActivity {
     TextView textView;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +29,32 @@ public class MyVolley extends AppCompatActivity {
         setContentView(R.layout.activity_my_volley);
 
         textView = findViewById(R.id.textView15);
+        imageView = findViewById(R.id.imageView2);
+
         Button button = findViewById(R.id.button36);
+        Button button2 = findViewById(R.id.button37);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sendRequest();
             }
         });
-
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendImageRequest();
+            }
+        });
         if (AppHelper_Volley.requestQueue == null) {
             AppHelper_Volley.requestQueue = Volley.newRequestQueue(getApplicationContext());
         }
+    }
+
+    public void sendImageRequest() {
+        String url = "https://postfiles.pstatic.net/MjAxOTAxMTJfNjgg/MDAxNTQ3MjcxMzM4MTE5.czRLtgOJlBHIoXrTxG0ZuOUM81qejXfYEEZVGwt5Bqgg.uF86xUeOcBCohhkbKgyqkdFz4NnCvWgUGor-IXzvQ6Qg.JPEG.tjdwkd2012/20171207_204602.jpg?type=w773";
+        ImageLoadTask task = new ImageLoadTask(url, imageView);
+        task.execute();
     }
 
     public void sendRequest() {
